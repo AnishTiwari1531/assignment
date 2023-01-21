8f143af8b99e9c55a9449d9a6bf35b7eb53361da
AnishTiwari1531 <anishtiwari1531@gmail.com>
2023-01-21 16:29:54 +0530

    Done

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,45 @@
+import Google from "../img/google.png";
+import Facebook from "../img/facebook.png";
+import { NavLink } from 'react-router-dom';
+
+const Signup = () => {
+  const google = () => {
+    window.open("http://localhost:5000/auth/google", "_self");
+  };
+
+  const facebook = () => {
+    window.open("http://localhost:5000/auth/facebook", "_self");
+  };
+
+  return (
+    <div className="login">
+      <h1 className="loginTitle">Choose a Registration Method</h1>
+      <div className="wrapper">
+        <div className="left">
+          <div className="loginButton google" onClick={google}>
+            <img src={Google} alt="" className="icon" />
+            Google
+          </div>
+          <div className="loginButton facebook" onClick={facebook}>
+            <img src={Facebook} alt="" className="icon" />
+            Facebook
+          </div>
+        </div>
+        <div className="center">
+          <div className="line" />
+          <div className="or">OR</div>
+        </div>
+        <div className="right">
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+          <button className="submit">Sign Up</button>
+          <div style={{margin: 20}}>
+            Already Have an account? <NavLink to="/login">Login</NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
